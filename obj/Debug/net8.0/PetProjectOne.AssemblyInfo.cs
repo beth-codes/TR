ade4295c64605bd281f8423947c98f12e053b56c
elizabeth.adegunwa <elizabeth.adegunwa@purplemedia.co.uk>
2025-01-27 20:13:33 +0000

    server setup

--- obj/Debug/net8.0/PetProjectOne.AssemblyInfo.cs
+++ obj/Debug/net8.0/PetProjectOne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetProjectOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029ecf5a92b67a6c22d0dca5a09421cc685eb33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetProjectOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetProjectOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
