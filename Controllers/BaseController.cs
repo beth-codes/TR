39b81fc86b832b37658dfece9f1eeb221acc6f41
Emmanuel Imion <Emmanuel.Imion@37DZDB3CX.Coronationmb.com>
2025-02-11 20:47:21 +0100

    updated login and added Generic response

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetProjectOne.Controllers
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        internal int GetStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                200 => StatusCodes.Status200OK,
+                400 => StatusCodes.Status400BadRequest,
+                401 => StatusCodes.Status401Unauthorized,
+                404 => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
